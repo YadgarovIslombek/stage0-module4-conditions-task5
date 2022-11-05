a101f8b1a36ccd7ca966af4517ca76b2d578b4e1
Islombek Yadgarov <islam.yadgarov.677@gmail.com>
2022-11-05 12:01:06 +0500

    "commit"

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -1,6 +1,15 @@
 package school.mjc.stage0.conditions.task5;
 
+import java.util.Scanner;
+
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        boolean rem;
+        rem=((year%4==0)&&((year%400==0)||(year%100!=0)));
+        if (rem){
+            System.out.println("leap");
+        }else{
+            System.out.println("not leap");
+        }
     }
 }
